2087a50a62021cc1b719067f7ab84a914aab33c1
dfx_17 <rossohinpavel@yandex.ru>
2025-10-18 21:34:29 +0300

    add prettier and first backend endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello');
+import express from "express";
+
+const app = express();
+
+app.get("/ping", (req, res) => {
+  res.send({ message: "pong" });
+});
+
+app.listen(8000, () => {
+  console.info("Listening at http://localhost:8000");
+});
