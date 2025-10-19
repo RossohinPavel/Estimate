a8f0b0f32cc852632b9b1aa81c6e411bc275696c
dfx_17 <rossohinpavel@yandex.ru>
2025-10-19 15:47:18 +0300

    replace backend lang to python

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+from typing import Literal
+from fastapi import FastAPI
+
+
+
+app = FastAPI(
+    title='EstimateAPI',
+    summary='API для приложения по составлению смет'
+)
+
+
+@app.get('/ping', status_code=200, tags=['ping'])
+async def ping() -> Literal['pong']:
+    """Пинг сервиса"""
+    return 'pong'
